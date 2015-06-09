fe4d91a859f1161140026b6273f71b78bc0d0d2a
Kardo <kardoj@gmail.com>
2015-06-09 15:42:22 +0300

    Server
    Server files

--- server/session_start.php
+++ server/session_start.php
@@ -2,5 +2,6 @@
 	error_reporting(-1);
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header("Content-type: application/json; charset=utf-8");
 	session_start();
 ?>
